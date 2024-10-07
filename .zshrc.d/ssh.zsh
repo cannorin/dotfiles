3307123633ee7b81dae4543d70d24a3444a14987
cannorin <cannorin@users.noreply.github.com>
2024-10-07 19:31:37 +0000

    Mon Oct  7 19:31:37 UTC 2024

--- .zshrc.d/ssh.zsh
+++ .zshrc.d/ssh.zsh
@@ -1,10 +1,12 @@
 if [ -f ~/.ssh-agent ]; then
-    . ~/.ssh-agent >/dev/null
-  fi
-if [ -z "$SSH_AGENT_PID" ] || ! kill -0 $SSH_AGENT_PID 2>/dev/null; then
-    ssh-agent > ~/.ssh-agent
-    . ~/.ssh-agent >/dev/null
+  . ~/.ssh-agent >/dev/null
 fi
-if [ -f "$HOME/.ssh/id_rsa" ]; then ssh-add ~/.ssh/id_rsa* > /dev/null 2>&1; fi
+if [ -z "$SSH_AGENT_PID" ] || ! kill -0 $SSH_AGENT_PID 2>/dev/null; then
+  ssh-agent > ~/.ssh-agent
+  . ~/.ssh-agent >/dev/null
+fi
+for ID_FILE in $HOME/.ssh/id_*; do
+  if [ -f "$ID_FILE" ]; then ssh-add $ID_FILE >/dev/null 2>&1; fi
+done
 
 
