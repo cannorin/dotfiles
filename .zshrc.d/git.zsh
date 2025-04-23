1387df47bce889974d44cf2c5f763e5307298d91
cannorin <cannorin@users.noreply.github.com>
2025-04-23 16:25:33 +0900

    Wed Apr 23 16:25:33 JST 2025

--- .zshrc.d/git.zsh
+++ .zshrc.d/git.zsh
@@ -31,7 +31,7 @@ function git-unignore() {
 
 function git-prune-branches() {
    git fetch -p
-   git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -d
+   git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -D
 }
 
 
