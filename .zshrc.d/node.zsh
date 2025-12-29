044206afc6a11f8660bca8f2cce37e3e455258ee
cannorin <cannorin@users.noreply.github.com>
2025-12-30 02:24:53 +0900

    Tue Dec 30 02:24:53 JST 2025

--- .zshrc.d/node.zsh
+++ .zshrc.d/node.zsh
@@ -2,6 +2,5 @@ export PATH="$HOME/.deno/bin:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modu
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-. "/home/alice/.deno/env"
+[ -s "$HOME/.deno/env" ]  && \. "$HOME/.deno/env" # This loads deno
 
